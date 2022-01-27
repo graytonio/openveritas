b06fe01a8c1225f6f869ddcf94e082e736082dfe
Grayton Ward <graytonio.ward@gmail.com>
2022-01-26 23:14:56 -0500

    Fixed missing error checks

--- server/controller/routes/errors.go
+++ server/controller/routes/errors.go
@@ -3,14 +3,16 @@ package routes
 import (
 	"log"
 	"net/http"
+	"reflect"
 	"strings"
 )
 
 func checkRouteError(err error, rw http.ResponseWriter) bool {
 	if err != nil {
 		log.Println(err.Error())
-
-		if strings.Contains(err.Error(), "duplicate key error") {
+		if strings.Contains(err.Error(), "no documents in result") {
+			return false
+		} else if strings.Contains(err.Error(), "duplicate key") {
 			rw.WriteHeader(http.StatusConflict)
 		} else {
 			rw.WriteHeader(http.StatusInternalServerError)
@@ -22,7 +24,9 @@ func checkRouteError(err error, rw http.ResponseWriter) bool {
 }
 
 func checkNotFound(data interface{}, rw http.ResponseWriter) bool {
-	if data == nil {
+	log.Printf("Checking if %v exists", data)
+	if data == nil || reflect.ValueOf(data).IsNil() {
+		log.Println("NOT FOUND")
 		rw.WriteHeader(http.StatusNotFound)
 		return true
 	}
