19ea674a54b7c43511ab0e832f538019afe16b43
Grayton Ward <g-buddy@comcast.net>
2022-02-20 12:47:25 -0500

    Fixed bad host property on get node property

--- build.sh
+++ build.sh
@@ -2,17 +2,22 @@
 
 server_package="veritas-server"
 client_package="veritas-client"
+parent_path=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)
 
 target=$1
 
 buildServer() {
+    cd "$parent_path"
+    mkdir -p ./dev-build
     echo "Building Server"
-    go build -o $server_package ./server
+    go build -o $server_package ./dev-build/server
 }
 
 buildCLI() {
+    cd "$parent_path"
+    mkdir -p ./dev-build
     echo "Building CLI"
-    go build -o $client_package ./cli
+    go build -o $client_package ./dev-build/cli
 }
 
 if [[ -z $target ]]; then
