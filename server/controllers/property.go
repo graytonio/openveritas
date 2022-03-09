9589ec8e49c2f540eb7c538495730420eae3e344
Grayton Ward <g-buddy@comcast.net>
2022-03-09 01:11:08 -0500

    Untested methods

--- server/controllers/property.go
+++ server/controllers/property.go
@@ -47,6 +47,20 @@ func GetNodePropertyByName(node *Node, prop_name string) (*Property, error) {
 	return property, nil
 }
 
+func QueryPropertyNames(query_string string) ([]Property, error) {
+	properties := []Property{}
+	PropertyCollection := mgm.Coll(&Property{})
+
+	searchStage := bson.D{{"$search", bson.D{{"wildcard", bson.D{{"path", "title"}, {"query", query_string}}}}}}
+
+	err := PropertyCollection.SimpleFind(&properties, searchStage)
+	if err != nil {
+		return nil, err
+	}
+
+	return properties, nil
+}
+
 func CreateProperty(prop *Property) error {
 	PropertyCollection := mgm.Coll(prop)
 	err := PropertyCollection.Create(prop)
